ae63f05bddf2a3239bda5cd578f0bf2cb2e81f75
nightwebworm <nightwebworm@gmail.com>
2022-04-30 20:53:08 +0300

    solution btn

--- main.js
+++ main.js
@@ -1,33 +1,46 @@
 const SIZE=3
 
-let puzzle = randomPuzzle(200);
-const original = copyPuzzle(puzzle)
-printGrid(original.grid)
-
-let solution = solveIt(puzzle);
-console.log(solution)
-
 window.onload = ()=>{
-    renderPuzzle(original)
+
+    let puzzle = randomPuzzle(100)
+    printGrid(puzzle.grid)
+    renderPuzzle(puzzle)
+
+    document.querySelector('#shuffle').addEventListener('click',()=>{
+        puzzle = randomPuzzle(100)
+        printGrid(puzzle.grid)
+        renderPuzzle(puzzle)
+    });
+    document.querySelector('#solve').addEventListener('click',()=>{
+        // Get the puzzle from Dom cause JS is butthurt
+        const grid = fromDomToGrid();
+        let puzzle = {
+            grid: grid,
+            path:'',
+            gen:0
+
+        }
+        puzzle.zero = findZero(puzzle.grid);
+        puzzle.code = getpuzzlecode(puzzle);
+        puzzle.eval = evaluate(puzzle);
+
+        console.log(puzzle)
+        const sol = solve(puzzle)
+        console.log(sol)
+    });
+
 }
 
-
-
-
-
-// render the array
-
-// moves 
-    // change the array
-    // render the array
-
-
-function solveIt(puzzle){
-    puzzle.zero = findZero(puzzle.grid);
-    puzzle.code = getpuzzlecode(puzzle);
-    puzzle.eval = evaluate(puzzle);
-    puzzle.path = '';
-    puzzle.gen=0;
-
-    return solve(puzzle)
+function fromDomToGrid(){
+    let grid = [];
+    let gridStr = document.querySelector('#container').dataset.puzzle;
+    gridStr=gridStr.replaceAll(',','');
+    for (var i = 0; i < gridStr.length; i++) {
+        let val = gridStr.charAt(i);
+        let x = i%3;
+        let y = Math.floor(i/3);
+        if(grid[y]==undefined)grid[y]=[];
+        grid[y][x]=val;
+    };
+    return grid;
 }
--- paint.js
+++ paint.js
@@ -6,14 +6,47 @@ function renderPuzzle(puzzle){
         for (let j=0;j<puzzle.grid.length;j++){
             if(puzzle.grid[i][j]!=0){
                 const square = document.createElement('div');
-                square.classList.add('piece')
+                square.classList.add('piece');
                 square.width = square.height = '100px';
-                square.innerHTML = puzzle.grid[i][j]
+                square.innerHTML = puzzle.grid[i][j];
                 square.style.top = i*100+ `px`;
                 square.style.left = j*100+`px`;
-                container.append(square)
+                const val = puzzle.grid[i][j];
+                square.dataset.val = val;
+                let pos = -(val%3)*100+"px" + " "+-Math.floor(val/3)*100 +"px";
+                square.style.backgroundPosition = pos;
+                container.append(square);
+
+                square.addEventListener('click', ()=>{
+                    moveEvent(square,puzzle,i,j)
+                })
+
+                function moveEvent(square,puzzle,i,j){
+                    UImove(square,puzzle,i,j)
+                }
+
             }
-            
         }
-    }
+    };
+    container.dataset.puzzle = puzzle.grid;
+};
+
+function UImove(square,puzzle,y,x){
+
+    let dx = x - puzzle.zero.x;
+    let dy = y - puzzle.zero.y;
+    if (Math.abs(dx)>1 || Math.abs(dx)>1 || (Math.abs(dx)+Math.abs(dy)>=2) ) return false;
+
+    const mov = move(puzzle,dy,dx);
+    if(mov!=null) {
+        const dir = addToPath(dy, dx);
+        square.classList.add(dir)
+        puzzle = mov;
+    };
+
+    setTimeout(()=>{
+        const dir = addToPath(dy, dx);
+        square.classList.remove(dir)
+        renderPuzzle(puzzle);
+    },100);
 }
